bb3ab35bf10a9f3295b82c7aa6d7c3141e71a85d
Viktorhjh <Viktorsek7@gmail.com>
2022-01-14 00:10:06 +0100

    Добавьте файлы проекта.

---
+++ lab 10/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace lab_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(12, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(400, 30);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(12, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(400, 30);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 147);
+            this.label1.MaximumSize = new System.Drawing.Size(400, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Nothing",
+            "0",
+            "1",
+            "2"});
+            this.comboBox1.Location = new System.Drawing.Point(323, 281);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(89, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Text = "Nothing";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "int",
+            "string",
+            "double",
+            "Person"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 120);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(120, 24);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.Text = "Nothing";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 282);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 25);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 282);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 25);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Create Person";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 316);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Lab 10";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ lab 10/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_10
+{
+    public partial class Form1 : Form
+    {
+        int index, indexer;      
+        List<Person> people = new List<Person>(3);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (index)
+            {
+                case 0:
+                    Trojka<int> trojkaInt = new Trojka<int>(int.Parse(textBox1.Text),
+                int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+                    label1.Text = trojkaInt.ToString();
+                    break;
+
+                case 1:
+                    Trojka<string> trojkaStr = new Trojka<string>(textBox1.Text,
+                textBox2.Text, textBox3.Text);
+                    label1.Text = trojkaStr.ToString();
+                    break;
+
+                case 2:
+                    Trojka<double> trojkaDouble = new Trojka<double>(double.Parse(textBox1.Text),
+                double.Parse(textBox2.Text), double.Parse(textBox3.Text));
+                    label1.Text = trojkaDouble.ToString();
+                    break;
+
+                case 3:
+                    Trojka<Person> trojkaPerson = new Trojka<Person>(people[0], people[1], people[2]);
+                    label1.Text = $"{trojkaPerson.A.name} {trojkaPerson.A.surname} {trojkaPerson.A.age}\n" +
+                        $"{trojkaPerson.B.name} {trojkaPerson.B.surname} {trojkaPerson.B.age}\n" +
+                        $"{trojkaPerson.C.name} {trojkaPerson.C.surname} {trojkaPerson.C.age}";
+                    break;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            label1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (people.Count < 3)
+            {
+                people.Add(new Person(textBox1.Text, textBox2.Text, int.Parse(textBox3.Text)));
+            }
+            else
+            {                
+                people.RemoveAt(people.Count);
+            }            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            indexer = comboBox1.SelectedIndex;            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            index = comboBox2.SelectedIndex;
+        }
+    }
+
+    public class Person
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int age { get; set; }
+        public Person(string n, string s, int a)
+        {
+            name = n;
+            surname = s;
+            age = a;
+        }
+    }
+
+    public class Trojka<T>
+    {
+        public T A { get; set; }
+        public T B { get; set; }
+        public T C { get; set; }
+
+        public Trojka(T a, T b, T c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public override string ToString()
+        {
+            return $"({A.ToString()}, {B.ToString()}, {C.ToString()})";
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0: return A;
+                    case 1: return B;
+                    case 2: return C;
+                    default: throw new Exception("Unknown index");
+                }                
+            }
+            set
+            {
+                switch (index)
+                {
+                    case 0:
+                        A = value;
+                        break;
+                    case 1:
+                        B = value;
+                        break;
+                    case 2:
+                        C = value;
+                        break;
+                }
+            }        
+        }
+    }
+}
+/*
+konstruktor z trzema parametrami,
+właściwości A, B, C, które pozwolą "dostać się" do tych trzech wartości,
+nadpisaną metodę ToString( ), która zwróci napis w postaci "(A, B, C)",
+indekser (trojka[0] => A, trojka[1] => B, trojka[2] => C),
+metodę Sort( ), która ustawi wartości w kolejności rosnącej.
+*/
